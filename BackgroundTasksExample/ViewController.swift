1bc3924666781c14b8465ae68d491f3b7dc74ab1
Pooyan J <pooyanjavidi@gmail.com>
2024-09-02 08:18:20 +0330

    Initial Commit

---
+++ BackgroundTasksExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BackgroundTasksExample
+//
+//  Created by Pooyan J on 6/12/1403 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
